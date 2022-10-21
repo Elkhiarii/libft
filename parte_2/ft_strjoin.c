e12459afd5714b095ab14d9856dc818a28d05ee2
Othmane elkhiari <othmaneelkhiari@gmail.com>
2022-10-21 09:39:36 +0100

    join

--- parte_2/ft_strjoin.c
+++ parte_2/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: oelkhiar <oelkhiar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 16:19:20 by oelkhiar          #+#    #+#             */
-/*   Updated: 2022/10/20 18:38:02 by oelkhiar         ###   ########.fr       */
+/*   Updated: 2022/10/21 09:37:57 by oelkhiar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*str;
-	size_t	lens1;
-	size_t	len;
 	int		i;
+	int		j;
+	char	*str;
 
+	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	i = 0;
-	lens1 = ft_strlen(s1);
-	len = lens1 + ft_strlen(s2);
-	str = (char *)malloc(len + 1);
+	j = 0;
 	if (!str)
 		return (0);
 	while (s1[i])
@@ -30,12 +28,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		str[i] = s1[i];
 		i++;
 	}
-	i = 0;
-	while (s2[i])
+	while (s2[j])
 	{
-		str[lens1 + i] = s2[i];
-		i++;
+		str[i + j] = s2[j];
+		j++;
 	}
-	str[lens1 + i] = '\0';
+	str[i + j] = '\0';
 	return (str);
 }
