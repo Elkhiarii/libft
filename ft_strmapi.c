f127e55598ebf6e87ecfe0ffeea9f4404ee39a64
Othmane elkhiari <othmaneelkhiari@gmail.com>
2022-10-21 15:00:19 +0100

    coding

---
+++ ft_strmapi.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oelkhiar <oelkhiar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/21 14:45:57 by oelkhiar          #+#    #+#             */
+/*   Updated: 2022/10/21 14:51:48 by oelkhiar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    unsigned int i;
+    char *str;
+
+    str = ft_strdup((char *)s);
+    i = 0;
+    if(!str)
+        return (0);
+    while(str[i])
+    {
+        str[i] = f(i,str[i]);
+        i++;
+    }
+}
