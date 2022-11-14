e7d280b1d7c89059a7b9d9913f25ccf8da76b549
Othmane elkhiari <othmaneelkhiari@gmail.com>
2022-11-14 12:00:50 +0100

    ll

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: oelkhiar <oelkhiar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 13:21:58 by oelkhiar          #+#    #+#             */
-/*   Updated: 2022/11/08 11:33:26 by oelkhiar         ###   ########.fr       */
+/*   Updated: 2022/11/14 11:58:06 by oelkhiar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	x = (unsigned char *)s1;
 	y = (unsigned char *)s2;
 	i = 0;
+	if (n == 0)
+		return (0);
 	while (x[i] && y[i] && x[i] == y[i] && i < n - 1)
 		i++;
 	return (x[i] - y[i]);
